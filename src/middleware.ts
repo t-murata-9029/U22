1ff8e7e0173ea4b3b3795be52f05b081a498203e
ToshihideMurata <t.murata.lol.9029@gmail.com>
2025-04-28 09:20:22 +0900

    コメント追記

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server';
 
+/*---リクエストからURLを取得できるようにヘッダーをいじる---*/
 export function middleware(request: Request) {
     const requestHeaders = new Headers(request.headers);
     requestHeaders.set('x-url', request.url);
