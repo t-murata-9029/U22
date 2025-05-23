f420d571d95c9bbe9b75b28d2926e28bdc4803eb
ToshihideMurata <t.murata.lol.9029@gmail.com>
2025-05-23 15:24:10 +0900

    wawawa

---
+++ src/app/user/signup/result/page.tsx
@@ -0,0 +1,9 @@
+import { Box, Typography } from "@mui/material";
+
+export default function () {
+    return (
+        <Box>
+            <Typography>アカウント作成できたよ</Typography>
+        </Box>
+    );
+}
