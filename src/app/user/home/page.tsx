67798f3863e6d97053d860f525e4da421a8a0202
ToshihideMurata <t.murata.lol.9029@gmail.com>
2025-06-03 15:56:14 +0900

    wa

--- src/app/user/home/page.tsx
+++ src/app/user/home/page.tsx
@@ -1,8 +1,8 @@
-'use client'
-
+import { sessionValidator } from "@/utils/sessionValidator";
 import { Box, Typography } from "@mui/material";
 
 export default function page() {
+    sessionValidator()
     return (
         <Box>
             <Typography>ここは、ユーザーのホームページです</Typography>
--- src/utils/sessionValidator.ts
+++ src/utils/sessionValidator.ts
@@ -4,17 +4,18 @@
  * DBと接続して検証するようにする
  */
 
+import { useRouter } from 'next/navigation';
 import { supabase } from "@/lib/supabase";
-import { redirect } from "next/navigation";
 
 /* sessionを検証するファイル */
 
 export async function sessionValidator() {
+    const router = useRouter();
     const data = await supabase.auth.getSession()
 
     /* sessionが存在するか確認する */
     if (data.data.session?.access_token == null) {
-        redirect('/user/login');
+        router.push('/user/login')
     }
 
     /* sessionが有効期限内か確認する */
@@ -23,6 +24,6 @@ export async function sessionValidator() {
     // 現在のUNIXタイムスタンプを秒単位に変換
     const current_timestamp_seconds = Math.floor(current_timestamp_ms / 1000);
     if (data.data.session?.expires_at == null || data.data.session?.expires_at < current_timestamp_seconds) {
-        redirect('/user/login');
+        router.push('/user/login')
     }
 }
