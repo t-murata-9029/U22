00be3b2d30f110358b1b89109b1351e0f0330f4f
ToshihideMurata <t.murata.lol.9029@gmail.com>
2025-06-03 10:17:30 +0900

    path取得するやつ

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
+const path = require("path");
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: config => {
+    // Vue と同じように 「@ = src/」,「~ = src/」に設定する。
+    // => モジュールのパス解決とエイリアスを設定している。
+    config.resolve.alias["@"] = path.resolve(__dirname, "src");
+    config.resolve.alias["~"] = path.join(__dirname, "src");
+    return config;
+  },
 };
 
 export default nextConfig;
