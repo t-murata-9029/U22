8f858448298776c8f65ee1f36a342a26c49eebf2
ToshihideMurata <t.murata.lol.9029@gmail.com>
2025-06-11 11:20:32 +0900

    無駄な行を削除

--- src/app/event/[event_id]/page.tsx
+++ src/app/event/[event_id]/page.tsx
@@ -1,10 +1,8 @@
 import EventTop from "@/features/routes/eventTop/components/EventTop";
-import { Typography } from "@mui/material";
 
 export default function page() {
     return (
         <>
-            <Typography variant='body1'>ここがイベントのトップ</Typography>
             <EventTop />
         </>
     );
