ca028404073004618cba93d3b09851897d4a8bc6
toshihide murata <t.murata.lol.9029@gmail.com>
2025-06-15 22:47:37 +0900

    a

--- src/app/user/home/page.tsx
+++ src/app/user/home/page.tsx
@@ -1,5 +1,3 @@
-import { sessionValidator } from "@/utils/sessionValidator";
-import { Box, Paper, Typography } from "@mui/material";
 import UserHome from "@/features/routes/userhome/components/Userhome";
 
 export default function page() {
--- src/features/routes/userhome/components/Userhome.tsx
+++ src/features/routes/userhome/components/Userhome.tsx
@@ -2,14 +2,51 @@
 
 import { supabase } from "@/lib/supabase";
 import { sessionValidator } from "@/utils/sessionValidator";
-import { Box, Paper, Typography, CircularProgress } from "@mui/material"; // Import CircularProgress for loading indicator
+import { Box, Paper, Typography, CircularProgress, Dialog, DialogTitle, Button } from "@mui/material"; // Import CircularProgress for loading indicator
 import { useEffect, useState } from "react";
 import QRCode from 'react-qr-code';
+
+
+export interface SimpleDialogProps {
+    userId: string;
+    open: boolean;
+    onClose: () => void;
+}
+
+function SimpleDialog(props: SimpleDialogProps) {
+    const { onClose, open } = props;
+
+    const handleClose = () => {
+        onClose();
+    };
+
+    const handleListItemClick = () => {
+        onClose();
+    };
+
+    return (
+        <Dialog onClose={handleClose} open={open}>
+            <DialogTitle>QRコード</DialogTitle>
+            <Box sx={{mx:2, my:2}}>
+                <QRCode value={props.userId || "no-session"} />
+            </Box>
+        </Dialog>
+    );
+}
 
 export default function UserHome() {
     const [inSession, setInSession] = useState(false);
     const [isLoading, setIsLoading] = useState(true); // New state for loading status
     const [userId, setUserId] = useState<string>(""); // State for QR code value
+    const [open, setOpen] = useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
 
     useEffect(() => {
         const validateSession = async () => {
@@ -44,13 +81,18 @@ export default function UserHome() {
     return inSession ? (
         <Box component={Paper} sx={{ p: 3, m: 2 }}>
             <Typography variant="h5" component="h1">ログインしてるよ</Typography>
-            <QRCode value={userId || "no-session"} />
+            
+            <Button variant="contained" onClick={handleClickOpen}>
+                QRコードを表示
+            </Button>
+            <SimpleDialog
+                userId={userId}
+                open={open}
+                onClose={handleClose}
+            />
         </Box>
-    ) : (
-        <Box component={Paper} sx={{ p: 3, m: 2 }}>
-            <Typography variant="h5" component="h1">ログインしてね</Typography>
-        </Box>
-    );
+    ) :
+        blockpage;
 }
 
 const blockpage = (
