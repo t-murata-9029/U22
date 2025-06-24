90ae009a7e4438283648ebc3c174b9a97fb88f5f
ToshihideMurata <t.murata.lol.9029@gmail.com>
2025-06-24 10:44:23 +0900

    おおもじさくじょ

---
+++ src/features/routes/signup/components/Signup.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import { Box, Button, TextField, Typography } from "@mui/material";
+import Form from 'next/form'
+import { signup } from '../endpoints'
+
+export default function Signup() {
+    return (
+        <Box>
+            <Typography variant="h5">新規アカウント作成</Typography>
+            <Form action={signup}>
+                <Box sx={{ py: 1 }}>
+                    <TextField name="email" label="メールアドレス" variant="outlined" />
+                </Box>
+                <Box sx={{ py: 1 }}>
+                    <TextField name="password" label="パスワード" variant="outlined" type="password" />
+                </Box>
+                <Box>
+                    <Button variant="contained" type="submit">作成</Button>
+                </Box>
+            </Form>
+        </Box>
+    );
+};
---
+++ src/features/routes/signup/endpoints.ts
@@ -0,0 +1,23 @@
+'use server'
+
+import { supabase } from "@/lib/supabase";
+import { redirect } from "next/navigation";
+
+export async function signup(formData: FormData) {
+    const email = formData.get('email')?.toString();
+    const password = formData.get('password')?.toString();
+
+    if (!email || !password) {
+        throw new Error('メールアドレスもしくはパスワードが未入力です。');
+    }
+
+    const { data, error } = await supabase.auth.signUp({
+        email, password, options: {
+            emailRedirectTo: 'http://localhost:3000/',
+        },
+    })
+
+    if (error) throw new Error(error.message)
+
+    redirect('/user/signup/result');
+}
