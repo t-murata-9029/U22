4f987c426f7fd5a2730c8885bd551ca9296c60a2
ToshihideMurata <t.murata.lol.9029@gmail.com>
2025-06-25 10:39:57 +0900

    空値を許すように変更

--- src/interfases/store.ts
+++ src/interfases/store.ts
@@ -2,9 +2,9 @@ import { ItemData } from "./item";
 
 export interface StoreData {
     id: string;
-    name: string;
-    image: string;
-    owner_id: string;
-    description: string;
-    item_list: ItemData[]
+    name?: string;
+    image?: string;
+    owner_id?: string;
+    description?: string;
+    item_list?: ItemData[]
 }
