43569f6e4ef03457331329ea3e2ad498e96a6510
ToshihideMurata <t.murata.lol.9029@gmail.com>
2025-06-25 10:42:01 +0900

    不要なコメントを削除

--- src/interfases/item.ts
+++ src/interfases/item.ts
@@ -1,8 +1,8 @@
 export interface ItemData {
-  id: string; // uuid
-  store_id?: string; // uuid
-  name?: string; // text
-  image?: string | null; // text (NULL許容)
-  price?: number; // int4
-  description?: string; // text
+  id: string;
+  store_id?: string;
+  name?: string;
+  image?: string | null;
+  price?: number;
+  description?: string;
 }
