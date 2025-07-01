e2d29e03d28bb5a9424b777c15d5e95e350f1afe
ToshihideMurata <t.murata.lol.9029@gmail.com>
2025-07-01 09:46:38 +0900

    try catch 削除

--- src/lib/supabase/middleware.ts
+++ src/lib/supabase/middleware.ts
@@ -3,44 +3,36 @@ import { createServerClient } from '@supabase/ssr';
 import { type NextRequest, NextResponse } from 'next/server';
 
 export async function updateSession(request: NextRequest) {
-  try {
-    let response = NextResponse.next({
-      request: {
-        headers: request.headers,
-      },
-    });
+  let response = NextResponse.next({
+    request: {
+      headers: request.headers,
+    },
+  });
 
-    const supabase = createServerClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL || '',
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '',
-      {
-        cookies: {
-          getAll() {
-            return request.cookies.getAll();
-          },
-          setAll(cookiesToSet) {
-            for (const { name, value } of cookiesToSet) {
-              response.cookies.set(name, value);
-            }
-            response = NextResponse.next({
-              request,
-            });
-            for (const { name, value, options } of cookiesToSet) {
-              response.cookies.set(name, value, options);
-            }
-          },
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL || '',
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '',
+    {
+      cookies: {
+        getAll() {
+          return request.cookies.getAll();
+        },
+        setAll(cookiesToSet) {
+          for (const { name, value } of cookiesToSet) {
+            response.cookies.set(name, value);
+          }
+          response = NextResponse.next({
+            request,
+          });
+          for (const { name, value, options } of cookiesToSet) {
+            response.cookies.set(name, value, options);
+          }
         },
       },
-    );
+    },
+  );
 
-    await supabase.auth.getUser();
+  await supabase.auth.getUser();
 
-    return response;
-  } catch (e) {
-    return NextResponse.next({
-      request: {
-        headers: request.headers,
-      },
-    });
-  }
+  return response;
 }
