978754edc6cc237d31c1fb5be11db333828e7cd2
ToshihideMurata <t.murata.lol.9029@gmail.com>
2025-07-01 15:50:43 +0900

    テスト

--- src/app/store/[storeid]/sales/page.tsx
+++ src/app/store/[storeid]/sales/page.tsx
@@ -3,9 +3,10 @@ import SalesHistory from '@/components/SalesHistory';
 import { useParams } from 'next/navigation';
 
 
+
 export default function Page() {
   const params = useParams();
-  const storeid = params.storeid as string; 
+  const storeid = params.storeid as string;
 
   // 取得した storeid をクライアントコンポーネントにpropsとして渡す
   return (
