d585abcfbdb639cb8dcdb02891e29a5b4f527bf4
syokapon <is.yuto7777@gmail.com>
2025-07-01 10:55:50 +0900

    競合

--- src/features/routes/userhome/endpoint.ts
+++ src/features/routes/userhome/endpoint.ts
@@ -44,11 +44,7 @@ export async function getJoinedEvents(userId: string | undefined): Promise<Event
  * @returns StoreData[]
  */
 export async function getJoinedStores(userId: string | undefined): Promise<StoreData[]> {
-<<<<<<< HEAD
-  const {  } = await supabase
-=======
   const { } = await supabase
->>>>>>> 96ebe9ab5c5fdfdf5e696ea4765bb93456c947a5
     .from('event_user_relation') // 参加者テーブル
     .select(`
         events (
