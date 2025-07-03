513ba780f30619d5b1f64ad07bdbf84b8fa8bd02
ToshihideMurata <t.murata.lol.9029@gmail.com>
2025-07-03 10:29:53 +0900

    relationのテーブルにインサートするように変更

--- src/features/routes/eventSignup/endpoint.ts
+++ src/features/routes/eventSignup/endpoint.ts
@@ -1,4 +1,5 @@
 import { supabase } from "@/lib/supabase";
+import { readdir } from "fs";
 
 /* イベント作成する関数 */
 export async function signup(formData: FormData) {
@@ -8,7 +9,7 @@ export async function signup(formData: FormData) {
     const userid = formData.get('userid')?.toString();
 
     // イベントをインサート
-    const { error } = await supabase
+    const { data: eventData, error: eventError } = await supabase
         .from('event') // 挿入するテーブル名
         .insert([
             {
@@ -17,10 +18,27 @@ export async function signup(formData: FormData) {
                 owner_id: userid,
                 email: "",
             },
+        ])
+        .select();
+
+    if (eventError || eventData == null) {
+        console.error("イベント作成処理でエラーが発生しました。");
+        return;
+    }
+
+    // 参加者としても登録
+    const { error: relationError } = await supabase
+        .from('event_user_relation') // 挿入するテーブル名
+        .insert([
+            {
+                event_id: eventData[0].id,
+                user_id: userid
+            },
         ]);
 
-    if (error) {
-        throw error;
+    if (relationError) {
+        console.error("イベント作成処理でエラーが発生しました。")
+        return;
     }
 
 }
